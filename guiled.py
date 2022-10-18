94887bece54f8f53c405218c65886b61fb25f994
theRTB <wreckernl@gmail.com>
2022-10-18 21:32:34 +0200

    created container class for variables to be edited and displayed in GUI and replaced constants with it

--- guiled.py
+++ guiled.py
@@ -16,17 +16,14 @@ from dragderivation import Trace, DragDerivation
 
 #TODO:
     #test hysteresis value
+    #create configuration file for constants
     #add frame to gui to edit constants in real time
-        #variables:
-            #illumination_interval
-            #reaction_time in frames (conversion in gui?)
-            #distance from revlimit frames
-            #distance from revlimit percent
-            #hysteresis percentage of revlimit
-            #state dropdown delay in frames
+    #move constants into objects
+        #name, value, gui_var, text, posttext, from/to lambdas
         #rewrite state triggers to use a full table
             #add full state trigger table to gui
         #dynamic led sizing?
+    #add audio tone to (different) reaction time adjusted shift rpm
             
         
     #blank shift leds after detecting gear change
@@ -42,13 +39,6 @@ AMBER = '#FFBF7F'
 RED   = '#FF8088'
 BLUE  = '#8080FF'
 
-ILLUMINATION_INTERVAL = int(2.0*60) #2.0 seconds
-REACTION_TIME = 12 #200 milliseconds
-DISTANCE_FROM_REVLIMIT_MS = 5 #83 milliseconds
-DISTANCE_FROM_REVLIMIT_ABS = .99 #99.2% of rev limit
-HYSTERESIS_PCT_REVLIMIT = 0.001 #0.1% of rev limit
-COUNTDOWN_MAX = 12 #dropping state only allowed after 12 frames
-
 STATES = [
     [BLACK]*10,
     [GREEN, GREEN] + [BLACK]*8,
@@ -58,7 +48,6 @@ STATES = [
     [BLUE]*10,
     [RED, BLUE, RED, BLUE, RED, RED, BLUE, RED, BLUE, RED] ]
 
-
 START_X = 0
 START_Y = 0
 LED_HEIGHT = 75
@@ -66,6 +55,46 @@ LED_WIDTH = 75
 LED_COUNT = 10
 HEIGHT = LED_HEIGHT
 WIDTH = LED_WIDTH*LED_COUNT
+
+class Variable():
+    def __init__(self, name, value, vartype, unit):
+        self.name = name
+        self.value = value
+        self.vartype = vartype
+        self.unit = unit
+
+    def init_tkintervar(self):
+        if self.vartype == 'String':
+            self.var = tkinter.StringVar(value=self.value)
+        elif self.vartype == 'Double':
+            self.var = tkinter.DoubleVar(value=self.value)
+        elif self.vartype == 'Int':
+            self.var = tkinter.IntVar(value=self.value)
+        
+#convenient class for displaying and modifying variables live in the GUI
+class V(): 
+    illumination_interval = Variable('Illumination interval', int(2.0*60), 'Int', 'frames')  #2.0 seconds
+    reaction_time = Variable('Reaction time', 12, 'Int', 'frames')  #200 milliseconds
+    distance_from_revlimit_ms = Variable('Distance from revlimit', 5, 'Int', 'frames')  #83 milliseconds
+    distance_from_revlimit_pct = Variable('Distance from revlimit', .99, 'Double', 'percent')  #99.0% of rev limit
+    hysteresis_pct_revlimit = Variable('Hysteresis', .001, 'Double', 'percent') #0.1% of rev limit
+    state_dropdown_delay = Variable('Shiftlight location x', 1532, 'Int', 'pixels')  #dropping state only allowed after 12 frames
+    shiftlight_x = Variable('Shiftlight location x', 1532, 'Int', 'pixels')
+    shiftlight_y = Variable('Shiftlight location y', 1763, 'Int', 'pixels')
+    
+    #initialize tkinter variable must be done after creating a tkinter root window
+    @classmethod 
+    def _init_tkintervariables(cls):
+        for name, value in cls.__dict__.items():
+            if name[0] == '_':
+                print(f"skipping {name}")
+                continue
+            print(f"working on {name}")
+            value.init_tkintervar()
+    
+    @classmethod
+    def _var_list(cls):
+        return [name for name in cls.__dict__.keys() if name[0] != '_']
 
 class GUILedDummy:
     def __init__(self, logger, root):
@@ -95,33 +124,27 @@ class GUILed:
         self.frame = None
         
         self.run_shiftleds = [False for x in range(11)]
-        self.lower_bound = [5000 for x in range(11)]
+        self.lower_bound = [5000 for x in range(11)] #these placeholder values mean nothing
         self.shiftrpm = [7000 for x in range(11)]
         self.unhappy_rpm = [7500 for x in range(11)]
-        self.hysteresis_rpm = HYSTERESIS_PCT_REVLIMIT*7500
+        self.hysteresis_rpm = V.hysteresis_pct_revlimit.value*7500
         
         self.step = [(self.shiftrpm[x] - self.lower_bound[x])/4 for x in range(11)]
         
         self.state = 0
         self.rpm = 0
         self.statedowntimer = 0
-        
-        # self.lower_bound_var = tkinter.StringVar()
-        # self.lower_bound_var.set("0000")
-        
-        # self.step_var = tkinter.StringVar()
-        # self.step_var.set("0000")
-        
-        self.rpm_var = tkinter.StringVar()
-        self.rpm_var.set("0000")
-        
+                
+        self.rpm_var = tkinter.StringVar(value='0000')
+            
         self.__init__window(root)
+        V._init_tkintervariables()
             
     def __init__window(self, root):
             self.root = root
             self.window = tkinter.Toplevel(root)
-            self.window.wm_attributes("-topmost", 1)
-            self.window.geometry(f"{WIDTH}x{HEIGHT}")
+            self.window.wm_attributes("-topmost", 1) #force always on top
+            self.window.geometry(f"{WIDTH}x{HEIGHT}+{V.shiftlight_x.value}+{V.shiftlight_y.value}")
             self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT, bg="#000000")
             self.canvas.pack()
             self.window.overrideredirect(True) #remove title bar, needs code to allow window to move
@@ -172,7 +195,7 @@ class GUILed:
     def set_rpmtable(self, rpmtable, rpmvalues, gears, revlimit, collectedingear, trace):
         self.logger.info(f"revlimit {revlimit} collectedingear {collectedingear}")
         
-        self.hysteresis_rpm = HYSTERESIS_PCT_REVLIMIT*revlimit
+        self.hysteresis_rpm = V.hysteresis_pct_revlimit.value*revlimit
         self.logger.info(f"hysteresis at {self.hysteresis_rpm} rpm steps")
         
         drag = DragDerivation(gears, final_drive=1, trace=trace)
@@ -194,20 +217,20 @@ class GUILed:
             self.logger.info(f"gear {gear} rpm {rpm}")
             
             #if at rev limit within x milliseconds, shift optimal shift point state to be x milliseconds away
-            adjusted_rpmlimit_ms = self.timeadjusted_rpm(DISTANCE_FROM_REVLIMIT_MS, revlimit, geardata[gear]['rpm'])
-            adjusted_rpmlimit_abs = int(revlimit*DISTANCE_FROM_REVLIMIT_ABS)
+            adjusted_rpmlimit_ms = self.timeadjusted_rpm(V.distance_from_revlimit_ms.value, revlimit, geardata[gear]['rpm'])
+            adjusted_rpmlimit_abs = int(revlimit*V.distance_from_revlimit_pct.value)
             self.logger.info(f"adjusted rpmlimit ms:{adjusted_rpmlimit_ms}, abs: {adjusted_rpmlimit_abs}")
             
             rpm = min(rpm, adjusted_rpmlimit_ms, adjusted_rpmlimit_abs)
             
             #at optimal shift rpm, we change state to 'past optimal' because humans have reaction time
             self.unhappy_rpm[gear] = rpm
-            rpm = self.timeadjusted_rpm(REACTION_TIME, rpm, geardata[gear]['rpm'])
+            rpm = self.timeadjusted_rpm(V.reaction_time.value, rpm, geardata[gear]['rpm'])
             self.logger.info(f"adjusted for reaction time {rpm}")
             
             for j, x in enumerate(geardata[gear]['rpm']):
                 if x >= rpm:
-                    offset = int(max(j - ILLUMINATION_INTERVAL, 0))
+                    offset = int(max(j - V.illumination_interval.value, 0))
                     self.lower_bound[gear] = int(geardata[gear]['rpm'][offset])
                     #print(f"j {j} val {rpmvalues[j]} offset {offset}")
                     break
@@ -235,17 +258,14 @@ class GUILed:
             state = 6
             
         if state < self.state:
-            if self.countdowntimer < COUNTDOWN_MAX:
+            if self.countdowntimer < V.state_dropdown_delay.value:
                 self.countdowntimer += 1
             else:
                 self.state = state
         else:
-            self.countdowntimer = COUNTDOWN_MAX
+            self.countdowntimer = V.state_dropdown_delay.value
             self.state = state
-            
-        # self.lower_bound_var.set(f"{self.lower_bound[fdp.gear]}")
-        # self.step_var.set(f"{self.step[fdp.gear]}")
-                    
+                                
         self.update_leds()
 
     def update_leds(self):
@@ -254,36 +274,38 @@ class GUILed:
             self.canvas.itemconfig(self.ledbar[i], fill=ledbar[i])
 
     def set_canvas(self, frame):
-        self.frame = tkinter.Canvas(frame, border=0, bg=constants.background_color, relief="groove",
+        self.frame = tkinter.Frame(frame, border=0, bg=constants.background_color, relief="groove",
                                             highlightthickness=True, highlightcolor=constants.text_color)
-
-        # tkinter.Label(self.frame, text="LED gearshifts", bg=constants.background_color, fg=constants.text_color,
-        #               font=('Helvetica 15 bold')).place(relx=0.2, rely=0.2, anchor=tkinter.CENTER)
-        # tkinter.Label(self.frame, textvariable=self.lower_bound_var, bg=constants.background_color, fg=constants.text_color,
-        #               font=('Helvetica 15 bold')).place(relx=0.5, rely=0.2, anchor=tkinter.CENTER)
-        # tkinter.Label(self.frame, textvariable=self.step_var, bg=constants.background_color, fg=constants.text_color,
-        #               font=('Helvetica 15 bold')).place(relx=0.8, rely=0.2, anchor=tkinter.CENTER)
-        tkinter.Label(self.frame, textvariable=self.rpm_var, bg=constants.background_color, fg=constants.text_color,
-                      font=('Helvetica 18 bold')).place(relx=0.8, rely=0.9, anchor=tkinter.CENTER)
         
-        # START_X = 250-80
-        # START_Y = 1
-        # LED_HEIGHT = 45
-        # LED_WIDTH = 80
-        # for i in range(10):
-        #     self.ledbar[i] = self.frame.create_rectangle(START_X,           START_Y+LED_HEIGHT*i, 
-        #                                                  START_X+LED_WIDTH, START_Y+LED_HEIGHT*(i+1), 
-        #                                                  fill='black', outline='white')
+        opts = {'bg':constants.background_color, 'fg':constants.text_color, 'font':('Helvetica 12')}
+        table = [{ 'text': 'Illumination interval', 'var':V.illumination_interval.var, 'posttext':'frames'},
+                 { 'text': 'Reaction time',         'var':V.reaction_time.var, 'posttext':'frames'},
+                 { 'text': 'Distance from revlimit', 'var':V.distance_from_revlimit_ms.var, 'posttext':'frames'},
+                 { 'text': 'Distance from revlimit', 'var':V.distance_from_revlimit_pct.var, 'posttext':'percent'},
+                 { 'text': 'Hysteresis, revlimit', 'var':V.hysteresis_pct_revlimit.var, 'posttext':'percent'},
+                 { 'text': 'State dropdown delay', 'var':V.state_dropdown_delay.var, 'posttext':'frames'},
+                 { 'text': 'Shiftlight location x', 'var':V.shiftlight_x.var, 'posttext':'pixels'},
+                 { 'text': 'Shiftlight location y', 'var':V.shiftlight_y.var, 'posttext':'pixels'}]
+        
+        for row, line in enumerate(table):
+            tkinter.Label(self.frame, text=line['text'], **opts).grid(row=row, column=0)
+            tkinter.Entry(self.frame, textvariable=line['var'], width=5, justify=tkinter.RIGHT, **opts).grid(row=row, column=1)
+            tkinter.Label(self.frame, text=line['posttext'], **opts).grid(row=row, column=2)            
+            
+        tkinter.Label(self.frame, textvariable=self.rpm_var, bg=constants.background_color, fg=constants.text_color,
+                      font=('Helvetica 18 bold')).grid(row=len(table))
+
         self.frame.pack(fill='both', expand=True)
-            #self.ledbar[i] = self.frame.create_rectangle(40, 1+45*i, 40+80, 1+50+45*i, fill='black', outline='white')
-            #self.ledbar[i] = self.frame.create_rectangle(10+30*i, 40,10+30+30*i,40+30, fill='black', outline='white')
     
     def reset(self):
         self.state = 0
-        self.dropdowntimer = COUNTDOWN_MAX
+        self.dropdowntimer = V.state_dropdown_delay.value
         self.update_leds()
         
         # self.lower_bound_var.set("0000")
         # self.step_var.set("0000")
         self.rpm_var.set("0000")
+        
+
+        
         
