18dd52a1b47fb7c12d439572bad9a83839c6824d
theRTB <wreckernl@gmail.com>
2022-12-28 08:06:23 +0100

    updated wheelsize display, for use at crawling speed with tickbox

--- guiwheelsize.py
+++ guiwheelsize.py
@@ -34,16 +34,22 @@ class GUIWheelsize:
     def __init__(self, logger):
         self.logger = logger
         
-        self.wheelsize_var = tkinter.StringVar()
-        self.wheelsize_var.set("00.00 00.00")
-        self.wheelsize_front = 0
-        self.wheelsize_rear = 0
-        self.wheelsize = {'front':deque(maxlen=300), 'rear':deque(maxlen=300)}
+     #   self.wheelsize_var = tkinter.StringVar()
+     #   self.wheelsize_var.set("00.00 00.00")
+        self.wheelsize_front_var = tkinter.StringVar(value="00.00")
+        self.wheelsize_rear_var = tkinter.StringVar(value="00.00")
+        self.wheelsize = {'front':deque(maxlen=600), 'rear':deque(maxlen=600)}
+        
+        self.tracking_var = tkinter.BooleanVar(value=True)
 
     def display(self):
-        self.wheelsize_var.set(f"{self.wheelsize_front * 100:.2f} {self.wheelsize_rear * 100:.2f}")
+        pass
+        #self.wheelsize_var.set(f"{self.wheelsize_front * 100:.2f} {self.wheelsize_rear * 100:.2f}")
 
     def update(self, fdp):
+        if not self.tracking_var.get():
+            return
+        
         diameter = {x:0 for x in ['FL', 'FR', 'RL', 'RR']}
         
         if fdp.steer != 0 or (fdp.wheel_rotation_speed_FL == 0 or
@@ -61,19 +67,34 @@ class GUIWheelsize:
         self.wheelsize['rear'].append(diameter['RL'])
         self.wheelsize['rear'].append(diameter['RR'])
         
-        self.wheelsize_front = statistics.median(self.wheelsize['front'])
-        self.wheelsize_rear = statistics.median(self.wheelsize['rear'])
+        #radius in centimeters
+        self.wheelsize_front_var.set(round(statistics.median(self.wheelsize['front'])*50, 2))
+        self.wheelsize_rear_var.set(round(statistics.median(self.wheelsize['rear'])*50, 2))
     
     def set_canvas(self, frame):
         self.frame = tkinter.Frame(frame, border=0, bg=constants.background_color, relief="groove",
                                    highlightthickness=True, highlightcolor=constants.text_color)
-        tkinter.Label(self.frame, text="wheelsize (cm)", bg=constants.background_color, fg=constants.text_color,
-                      font=('Helvetica 15 bold')).pack()
-        tkinter.Label(self.frame, textvariable=self.wheelsize_var, bg=constants.background_color,
-                      fg=constants.text_color, font=('Helvetica 20 bold italic')).pack()
-    
+        opts = {'bg':constants.background_color, 'fg':constants.text_color, 'font':('Helvetica 13 bold')}
+        row = 1
+        tkinter.Label(self.frame, text='Wheel radius (cm)', **opts).grid(row=row, column=1, columnspan=2)
+        
+        row += 1  
+        tkinter.Label(self.frame, text='Front', **opts).grid(row=row, column=1)
+        tkinter.Label(self.frame, text='Rear', **opts).grid(row=row, column=2)
+        
+        row += 1 
+        opts['font'] = ('Helvetica 18 bold')
+        tkinter.Label(self.frame, textvariable=self.wheelsize_front_var, width=5, **opts).grid(row=row, column=1, sticky=tkinter.E)
+        tkinter.Label(self.frame, textvariable=self.wheelsize_rear_var, width=5, **opts).grid(row=row, column=2, sticky=tkinter.E)
+        
+        row += 1 
+        tkinter.Checkbutton(self.frame, text='Tracking', 
+                            variable=self.tracking_var, 
+                            bg=constants.background_color, 
+                            fg=constants.text_color).grid(
+                                        row=row, column=1, columnspan=2)
+        
     def reset(self):
-        self.wheelsize = {'front':deque(maxlen=300), 'rear':deque(maxlen=300)}
-        self.wheelsize_front = 0
-        self.wheelsize_rear = 0
-        self.wheelsize_var.set("00.00 00.00")
+        self.wheelsize = {'front':deque(maxlen=600), 'rear':deque(maxlen=600)}
+        self.wheelsize_front_var.set("00.00")
+        self.wheelsize_rear_var.set("00.00")
