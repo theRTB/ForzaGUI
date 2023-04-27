e40e2dc3e364ece1b3c968491ea72b777598410d
theRTB <wreckernl@gmail.com>
2023-04-27 10:07:48 +0200

    simplified guibasic.py

--- guibasic.py
+++ guibasic.py
@@ -14,7 +14,7 @@ class GUIBasic:
     def __init__(self, logger, *args, **kwargs):
         self.logger = logger
 
-        self.acceleration_var = tkinter.StringVar()
+        self.throttle_var = tkinter.StringVar()
         self.brake_var = tkinter.StringVar()
         self.steer_var = tkinter.StringVar()
         
@@ -24,30 +24,25 @@ class GUIBasic:
         pass
 
     def update(self, fdp):
-        self.acceleration_var.set(f"{str(round(fdp.accel / 255 * 100, 1))}%")
+        self.throttle_var.set(f"{str(round(fdp.accel / 255 * 100, 1))}%")
         self.brake_var.set(f"{str(round(fdp.brake / 255 * 100, 1))}%")
         self.steer_var.set(f"{fdp.steer}")
     
+    def tkinterLabel(self, text, variable):
+        opts = {'bg':constants.background_color, 'fg':constants.text_color}
+        tkinter.Label(self.frame, text=text, font=('Helvetica 15 bold'), **opts).pack()
+        tkinter.Label(self.frame, textvariable=variable, width=6, 
+                      anchor=tkinter.E, font=('Helvetica 35 bold italic'), **opts).pack()
+
     def set_canvas(self, frame):
         self.frame = tkinter.Frame(frame, border=0, bg=constants.background_color, relief="groove",
                                    highlightthickness=True, highlightcolor=constants.text_color)
 
-        opts = {'bg':constants.background_color, 'fg':constants.text_color}
-        # place acceleration information text
-        tkinter.Label(self.frame, text="Accel", font=('Helvetica 15 bold'), **opts).pack()
-        tkinter.Label(self.frame, textvariable=self.acceleration_var, width=6, 
-                      anchor=tkinter.E, font=('Helvetica 35 bold italic'), **opts).pack()
-
-        # place brake information test
-        tkinter.Label(self.frame, text="Brake", **opts, font=('Helvetica 15 bold')).pack()
-        tkinter.Label(self.frame, textvariable=self.brake_var, width=6, 
-                      anchor=tkinter.E, font=('Helvetica 35 bold italic'), **opts).pack()
-        
-        tkinter.Label(self.frame, text="Steer", **opts, font=('Helvetica 15 bold')).pack()
-        tkinter.Label(self.frame, textvariable=self.steer_var, width=6, 
-                      anchor=tkinter.E, font=('Helvetica 30 bold italic'), **opts).pack()
+        self.tkinterLabel('Throttle', self.throttle_var)
+        self.tkinterLabel('Brake', self.brake_var)
+        self.tkinterLabel('Steer', self.steer_var)
         
     def reset(self):
-        self.acceleration_var.set("0.0%")
+        self.throttle_var.set("0.0%")
         self.brake_var.set("0.0%")
         self.steer_var.set("0")
